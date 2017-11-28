6ec70d6b3db3f1f42de9f0435645d539220a0a53
Yurina Shin <yurinas@gmail.com>
2017-11-28 20:07:52 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,7 +1,7 @@
 def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
       "I LOVE YOU TOO PUMPKIN!"
-    elsif phrase == "WHAT DID YOU EAT TODAY?" && phrase == "WHAT?"
+    elsif phrase == "WHAT DID YOU EAT TODAY?" || phrase == "WHAT?"
       "NO, NOT SINCE 1938!"
 
 else

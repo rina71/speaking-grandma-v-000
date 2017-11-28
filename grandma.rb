362206d1391f83616ad42280a493b393e981b57f
Yurina Shin <yurinas@gmail.com>
2017-11-28 20:07:26 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,11 +1,11 @@
 def speak_to_grandma(phrase)
-  if phrase == !(phrase.upcase)
-      "HUH?! SPEAK UP, SONNY!"
+  if phrase == "I LOVE YOU GRANDMA!"
+      "I LOVE YOU TOO PUMPKIN!"
     elsif phrase == "WHAT DID YOU EAT TODAY?" && phrase == "WHAT?"
       "NO, NOT SINCE 1938!"
 
 else
-"I LOVE YOU TOO PUMPKIN!"
+"HUH?! SPEAK UP, SONNY!"
   end
 end
 

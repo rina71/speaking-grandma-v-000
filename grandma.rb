0c998027bf67002b3e797758ad380f5c344a1651
Yurina Shin <yurinas@gmail.com>
2017-11-28 20:06:11 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,5 +1,5 @@
 def speak_to_grandma(phrase)
-  if phrase == !phrase.upcase
+  if phrase == !(phrase.upcase)
       "HUH?! SPEAK UP, SONNY!"
     elsif phrase == "WHAT DID YOU EAT TODAY?" && phrase == "WHAT?"
       "NO, NOT SINCE 1938!"
